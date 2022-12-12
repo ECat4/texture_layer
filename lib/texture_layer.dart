4308ad7c8e4491087de2ebcb370ba64a3c0b5abf
Kloss <yinbaodian@ybddeiMac.local>
2022-12-12 14:04:54 +0800

    fix: bug

--- lib/texture_layer.dart
+++ lib/texture_layer.dart
@@ -5,8 +5,8 @@ import 'package:flutter/services.dart';
 class TextureLayer {
   static const MethodChannel _channel = MethodChannel('texture_layer');
 
-  static Future<String?> get createTexture async {
-    final String? code = await _channel.invokeMethod('createTexture');
+  static Future<int?> get createTexture async {
+    final int? code = await _channel.invokeMethod('createTexture');
     return code;
   }
 }
